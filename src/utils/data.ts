3af9a9090645f19ba34e84a7ee5cab64ab6f90f4
lemaufo <leibertrejo8@gmail.com>
2025-06-16 13:29:59 -0600

    Update About Us section

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -13,7 +13,7 @@ const services = [
     },
     {
         title: "Sitios Web | e-Commerce",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        description: "Diseño y desarrollo de sitios web y tiendas en línea profesionales, optimizados para dispositivos móviles y pensados para convertir visitantes en clientes.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
