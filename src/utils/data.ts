50ed4a6ee4439ca83fccd0c91ac5818102440c13
lemaufo <leibertrejo8@gmail.com>
2025-06-16 14:50:03 -0600

    Add CTA section and images for Services section

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,39 +1,45 @@
 const services = [
     {
         title: "Software para administrar tu negocio",
-        description: "Ofrecemos venta de software líder en el mercado, como Microsip, Exsim y Rutax.",
+        description: "Ofrecemos venta de software líder en el mercado, como Microsip, Exsim y más.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/mantenimiento.png",
     },
     {
         title: "Equipos de cómputo y accesorios",
         description: "Contamos con equipos de cómputo y accesorios de alta calidad para satisfacer tus necesidades tecnológicas.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/equipo-de-computo.png",
     },
     {
         title: "Sitios Web | e-Commerce",
-        description: "Diseño y desarrollo de sitios web y tiendas en línea profesionales, optimizados para dispositivos móviles y pensados para convertir visitantes en clientes.",
+        description: "Diseño y desarrollo de sitios web y tiendas en línea profesionales, optimizados para móviles y pensados para convertir visitantes en clientes.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/sitios-web.png",
     },
     {
         title: "Instalación y configuración de redes",
         description: "Ofrecemos instalación y configuración de redes confiables para entornos domésticos y empresariales.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/redes.png",
     },
     {
         title: "Mantenimiento preventivo y correctivo a equipos de cómputo",
-        description: "Ofrecemos servicio de mantenimiento preventivo y correctivo a equipos de cómputo, garantizando un rendimiento óptimo y confiable.",
+        description: "Ofrecemos servicio de mantenimiento preventivo y correctivo a equipos de cómputo para un rendimiento óptimo.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/mantenimiento.png",
     },
     {
         title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate cempre ascendis nua.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
+        image: "/images/mantenimiento.png",
     },
 ];
 
