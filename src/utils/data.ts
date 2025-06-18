c850bd722d6099753cd890a6f6b7d8da56c72051
lemaufo <leibertrejo8@gmail.com>
2025-06-18 13:54:36 -0600

    Update services images and add description for new service

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,7 +4,7 @@ const services = [
         description: "Ofrecemos venta de software líder en el mercado, como Microsip, Exsim y más.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-        image: "/images/mantenimiento.png",
+        image: "/images/software-de-negocios.png",
     },
     {
         title: "Equipos de cómputo y accesorios",
@@ -35,11 +35,11 @@ const services = [
         image: "/images/mantenimiento.png",
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate cempre ascendis nua.",
+        title: "Cámaras de seguridad y videovigilancia.",
+        description: "Instalación y configuración de sistemas de videovigilancia para seguridad y control en hogares y negocios.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-        image: "/images/mantenimiento.png",
+        image: "/images/camaras-y-videovigilancia.png",
     },
 ];
 
